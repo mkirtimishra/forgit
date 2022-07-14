c2b0848529da12c66db0008c3ded55ae6b31a5c1
Kirti_mishra <miskirti9005@gmail.com>
2022-07-14 08:46:51 +0530

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void sum(int a, int b){
+        int s=a+b;
+        System.out.println(s);
+    }
+    public static void main(String[] args){
+        int a=10;
+        int b=20;
+        sum(a,b);
+    }
+}
